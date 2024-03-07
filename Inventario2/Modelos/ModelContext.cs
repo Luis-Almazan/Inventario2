ff73c0c87b282c339a81e08715cab54d011092d4
Luis Almazan <luis.almaza.garcia@gmail.com>
2024-03-06 21:37:46 -0600

    Codificacion de String conexion

--- Inventario2/Modelos/ModelContext.cs
+++ Inventario2/Modelos/ModelContext.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
-
+using Microsoft.Extensions.Configuration;
 namespace Inventario2.Modelos
 {
     public partial class ModelContext : DbContext
@@ -10,7 +10,7 @@ namespace Inventario2.Modelos
         public ModelContext()
         {
         }
-
+        private readonly IConfiguration _configuration;
         public ModelContext(DbContextOptions<ModelContext> options)
             : base(options)
         {
@@ -34,7 +34,8 @@ namespace Inventario2.Modelos
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseOracle("User Id=ARQUI;Password=Arqui123;Data Source=192.168.1.94:1521/xe;");
+                string connectionString = _configuration.GetConnectionString("MiConexion");
+                optionsBuilder.UseOracle(connectionString);
             }
         }
 
